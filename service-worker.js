bfb1700c6740dfe1e0f0a44e94c50fcfc2519c1a
Ziggy Qiao <zige.qiao@gmail.com>
2026-09-04 22:46:27 +0100

    Add window opening plan

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dew-v1";
+const CACHE_NAME = "is-it-dryer-out-v2";
 const APP_FILES = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
 
 self.addEventListener("install", (event) => {
