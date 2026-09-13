c8d6fee7bfbf150bb913ee7388387b9b576d6e50
Ziggy Qiao <zige.qiao@gmail.com>
2026-09-13 15:51:17 +0100

    Refine v0.4.1 plan and dashboard

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "is-it-dryer-out-v93";
+const CACHE_NAME = "is-it-dryer-out-v97";
 const APP_FILES = [
   "./",
   "index.html",
-  "styles.css?v=93",
-  "app.js?v=93",
+  "styles.css?v=97",
+  "app.js?v=97",
   "manifest.webmanifest",
   "favicon-v4.png",
 ];
